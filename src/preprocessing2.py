ce0313e249c8f735cf36f2e7f228c3408a8a0ab0
mecha2k <mecha2k@naver.com>
2021-10-02 09:23:05 +0900

    preprocessing

--- src/preprocessing2.py
+++ src/preprocessing2.py
@@ -23,6 +23,8 @@ plt.rcParams["figure.titlesize"] = 24
 plt.rcParams["figure.dpi"] = 300
 plt.rcParams["figure.figsize"] = [12, 8]
 
+pd.set_option("display.precision", 2)
+
 if __name__ == "__main__":
     src_data = "./data/stock1.pkl"
     tickers = {"현대차": "A005380", "삼성전자": "A005930", "네이버": "A035420", "카카오": "A035720"}
@@ -61,53 +63,30 @@ if __name__ == "__main__":
     plt.legend()
     plt.savefig("images/stocks02.png", bbox_inches="tight")
 
+    data = data.copy().loc["현대차"]["2020-01":"2020-06"]
+    data = data.close.to_frame()
+    data.info()
+
     lags = [1, 5, 10, 20, 40, 60]
-    returns = data.groupby(level="ticker").close.pct_change()
+    returns = data.close.pct_change()
     percentiles = [0.0001, 0.001, 0.01]
     percentiles += [1 - p for p in percentiles]
-
-    ic(
-        returns.describe(percentiles=percentiles).iloc[2:]
-        # .to_frame("percentiles")
-        # .style.format(lambda x: f"{x:,.2%}")
-    )
+    print(returns.describe(percentiles=percentiles))
 
     q = 0.0001
     for lag in lags:
         data[f"return_{lag}d"] = (
-            data.groupby(level="ticker")
-            .close.pct_change(lag)
+            data.close.pct_change(lag)
             .pipe(lambda x: x.clip(lower=x.quantile(q), upper=x.quantile(1 - q)))
             .add(1)
             .pow(1 / lag)
             .sub(1)
         )
-
     for t in [1, 2, 3, 4, 5]:
         for lag in [1, 5, 10, 20]:
-            data[f"return_{lag}d_lag{t}"] = data.groupby(level="ticker")[f"return_{lag}d"].shift(
-                t * lag
-            )
-
+            data[f"return_{lag}d_lag{t}"] = data[f"return_{lag}d"].shift(t * lag)
     for t in [1, 5, 10, 20]:
-        data[f"target_{t}d"] = data.groupby(level="ticker")[f"return_{t}d"].shift(-t)
+        data[f"target_{t}d"] = data[f"return_{t}d"].shift(-t)
     ic(data.head())
 
-    data.iloc[:100].to_csv("data/price_lag.csv")
-
-    # data = data.join(stocks[["sector"]])
-    # data["year"] = data.index.get_level_values("date").year
-    # data["month"] = data.index.get_level_values("date").month
-    # data.info(show_counts=True)
-    # data.assign(sector=pd.factorize(data.sector, sort=True)[0]).to_hdf(
-    #     "../data/lin_models.h5", "model_data/no_dummies"
-    # )
-    # data = pd.get_dummies(
-    #     data,
-    #     columns=["year", "month", "sector"],
-    #     prefix=["year", "month", ""],
-    #     prefix_sep=["_", "_", ""],
-    #     drop_first=True,
-    # )
-    # data.info(show_counts=True)
-
+    data.to_csv("data/price_lag.csv")
