4118cb3ce43bca1b8dfb2b31da3c8002f3e89db9
mecha2k <mecha2k@naver.com>
2021-11-23 06:37:29 +0900

    marcap

--- src/marcap_api.py
+++ src/marcap_api.py
@@ -1,23 +1,62 @@
+import pandas as pd
+from pandas.io.formats.format import return_docstring
 from marcap import marcap_data
 from datetime import datetime
 
+marcap_ind = {
+    "Code": "종목코드",
+    "Name": "종목",
+    "Close": "종가",
+    "Volume": "거래량",
+    "Amount": "거래대금",
+    "Marcap": "시가총액(백만원)",
+    "Stocks": "상장주식수",
+    "Market": "시장",
+}
+
+
+def get_marcap_period(start, end):
+    data = list()
+    keys = list()
+    for date in pd.date_range(start=start, end=end, freq="AS"):
+        date = datetime(date.year, 4, 1)
+        df = marcap_data(start=date)
+        df = df[list(marcap_ind.keys())].reset_index(drop=True)
+        data.append(df)
+        keys.append(date)
+
+    return pd.concat(data, keys=keys)
+
 
 if __name__ == "__main__":
-    ticker = "005930"
+    start = datetime(2000, 1, 1)
+    end = datetime(2021, 12, 31)
 
-    start = datetime(2019, 1, 1)
-    end = datetime(2020, 12, 31)
+    # df = get_marcap_period(start=start, end=end)
+    # df.to_pickle("data/marcap_period.pkl")
 
+    df = pd.read_pickle("data/marcap_period.pkl")
+    print(f"{len(df):,}")
+
+    df = df.loc[df["Code"].str.endswith("0")]
+    df = df.loc[df["Market"] == "KOSPI"]
+    print(f"{len(df):,}")
+
+    idx = pd.IndexSlice
+    for dt in pd.date_range(start=start, end=end, freq="AS"):
+        data = df.loc[idx[dt.year, :], :]
+        # print(f"{dt.year} : {len(data):,}")
+
+    # vol_quantile = df["Volume"].quantile(q=0.3, interpolation="linear")
+    # df = df.loc[df["Volume"] > vol_quantile]
+
+    # ticker = "005930"
     # df = marcap_data("2020-01-01", "2020-12-31")
-    df = marcap_data(start=start, end=end, code=ticker)
-    df = df.resample(rule="Y").last()
-    df.to_csv("data/marcap.csv", encoding="utf-8-sig")
+    # df = marcap_data(start=start, end=end, code=ticker)
+    # df = df.resample(rule="Y").last()
+    # df.to_csv("data/marcap.csv", encoding="utf-8-sig")
 
-    close = int(df.loc["2020", "Close"])
-    marketcap = int(df.loc["2020", "Marcap"])
-    stock_tot = int(marketcap / close)  # 5969782550
-    print(close, marketcap, stock_tot)
-
-    df = marcap_data("2021-11-10")
-    print(df.info())
-    print(df.head())
+    # close = int(df.loc["2020", "Close"])
+    # marketcap = int(df.loc["2020", "Marcap"])
+    # stock_tot = int(marketcap / close)  # 5969782550
+    # print(close, marketcap, stock_tot)
