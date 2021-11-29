615a65a6d92bfd9466ea28fac2d56c84750ac7ba
mecha2k <mecha2k@naver.com>
2021-11-29 18:58:03 +0900

    quantstats

--- src/quantstats/quantstats_api.py
+++ src/quantstats/quantstats_api.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     qs.extend_pandas()
 
     stock = qs.utils.download_returns("FB")
-    stock.plot_earnings(savefig="results/fb_earnings.png", start_balance=10000)
+    print(stock)
+    stock.plot_earnings(savefig="quantstats/results/fb_earnings.png", start_balance=10000)
     qs.stats.sharpe(stock)
     print(dir(stock))
     print(stock.sharpe())
@@ -18,7 +19,7 @@ if __name__ == "__main__":
 
     qs.reports.plots(stock, mode="basic")
     qs.reports.metrics(stock, mode="basic")
-    qs.reports.html(stock, "AAPL", output="results/quantstats-aapl.html")
+    qs.reports.html(stock, "AAPL", output="quantstats/results/quantstats-aapl.html")
 
     stat_func = [f for f in dir(qs.stats) if f[0] != "_"]
     plot_func = [f for f in dir(qs.plots) if f[0] != "_"]
